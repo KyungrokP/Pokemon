ace964e33e4cabf37230a375f54111e232c672b6
Kyungrok <kyungrok.park@mail.utoronto.ca>
2024-04-18 01:15:42 +0000

    update data_cleaning script

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,8 +1,8 @@
 #### Preamble ####
 # Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Kyungrok Park
+# Date: 1 April 2024
+# Contact: kyungrok.park@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
@@ -10,35 +10,30 @@
 #### Workspace setup ####
 library(tidyverse)
 
-#### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+#### Clean and save data for R&D budget dataset ####
+raw_data <- read_csv("data/raw_data/raw_research_budget.csv")
+selected_data <- raw_data[1:24, c("REF_DATE", "VALUE")]
+colnames(selected_data) <- c("year", "amount_in_mils")
+write_csv(selected_data, "data/analysis_data/research_budget.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+### Clean and save data for GNI dataset ###
+raw_data2 <- read_csv("data/raw_data/GNI_data.csv")
+selected_data2 <- raw_data2[raw_data2$Estimates == "Real gross national income, volume index 2017=100", ]
+selected_data2 <- selected_data2[c("REF_DATE", "VALUE")]
+colnames(selected_data2) <- c("date", "gni_in_2017volume")
+selected_data4$year <- substr(selected_data4$date, 1, 4)
+selected_data4$gni_in_2017volume <- as.numeric(selected_data4$gni_in_2017volume)
+yearly_sum <- aggregate(gni_in_2017volume ~ year, selected_data4, sum)
+write_csv(yearly_sum, "data/analysis_data/yearly_GNI_data.csv")
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+### Clean and save data for GDP dataset ###
+raw_data3 <- read_csv("data/raw_data/GDP_data.csv")
+selected_data3 <- raw_data3[1:96, c("REF_DATE", "VALUE")]
+colnames(selected_data3) <- c("date", "amount_in_mils")
+write_csv(selected_data3, "data/analysis_data/GDP_data.csv")
+
+selected_data3$year <- substr(selected_data3$date, 1, 4)
+selected_data3$amount_in_mils <- as.numeric(selected_data3$amount_in_mils)
+yearly_sum2 <- aggregate(amount_in_mils ~ year, selected_data3, sum)
+write_csv(yearly_sum2, "data/analysis_data/yearly_GDP_data.csv")
+
